007a6a2a25bf590982823fe634710f4fced65378
David Broder-Rodgers <broder93@gmail.com>
2017-02-12 21:38:43 +0000

    Better test naming

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -14,119 +14,119 @@ namespace GildedRose
         }
 
         [Test]
-        public void NameUnchanging()
+        public void Name_Unchanging()
         {
             var items = SetupTest("foo", 1, 1);
             Assert.AreEqual("foo", items[0].Name);
         }
 
         [Test]
-        public void SellInDecreases()
+        public void SellIn_Decreases()
         {
             var items = SetupTest("foo", 1, 1);
             Assert.AreEqual(0, items[0].SellIn);
         }
 
         [Test]
-        public void QualityDecreases()
+        public void Quality_Decreases()
         {
             var items = SetupTest("foo", 1, 1);
             Assert.AreEqual(0, items[0].Quality);
         }
 
         [Test]
-        public void QualityDecreasesTwiceAsFastIfSellInZero()
+        public void Quality_ForZeroSellIn_DecreasesTwiceAsFast()
         {
             var items = SetupTest("foo", 0, 2);
             Assert.AreEqual(0, items[0].Quality);
         }
 
         [Test]
-        public void QualityDecreasesTwiceAsFastIfSellInNegative()
+        public void Quality_ForNegativeSellIn_DecreasesTwiceAsFast()
         {
             var items = SetupTest("foo", -1, 2);
             Assert.AreEqual(0, items[0].Quality);
         }
 
         [Test]
-        public void QualityNeverNegative()
+        public void Quality_NeverNegative()
         {
             var items = SetupTest("foo", 1, 0);
             Assert.AreEqual(0, items[0].Quality);
         }
 
         [Test]
-        public void AgedBrieQualityIncreases()
+        public void Quality_ForAgedBrie_Increases()
         {
             var items = SetupTest(GildedRose.AGED_BRIE, 1, 1);
             Assert.AreEqual(2, items[0].Quality);
         }
 
         [Test]
-        public void QualityCannotExceedFifty()
+        public void Quality_CannotExceedFifty()
         {
             var items = SetupTest(GildedRose.AGED_BRIE, 1, 50);
             Assert.AreEqual(50, items[0].Quality);
         }
 
         [Test]
-        public void SulfurasQualityConstant()
+        public void Quality_ForSulfuras_IsConstant()
         {
             var items = SetupTest(GildedRose.SULFURAS, 1, 80);
             Assert.AreEqual(80, items[0].Quality);
         }
 
         [Test]
-        public void SulfurasSellInConstant()
+        public void SellIn_ForSulfuras_IsConstant()
         {
             var items = SetupTest(GildedRose.SULFURAS, 1, 80);
             Assert.AreEqual(1, items[0].SellIn);
         }
 
         [Test]
-        public void BackstagePassesQualityIncreases()
+        public void Quality_ForBackstagePasses_Increases()
         {
             var items = SetupTest(GildedRose.BACKSTAGE_PASS, 11, 1);
             Assert.AreEqual(2, items[0].Quality);
         }
 
         [Test]
-        public void BackstagePassesQualityIncreasesByTwoIfLessThanTen()
+        public void Quality_ForBackstagePassesAndSellInLessThanTen_IncreasesByTwo()
         {
             var items = SetupTest(GildedRose.BACKSTAGE_PASS, 10, 1);
             Assert.AreEqual(3, items[0].Quality);
         }
 
         [Test]
-        public void BackstagePassesQualityIncreasesByThreeIfLessThanFive()
+        public void Quality_ForBackstagePassesAndSellInLessThanFive_IncreasesByThree()
         {
             var items = SetupTest(GildedRose.BACKSTAGE_PASS, 5, 1);
             Assert.AreEqual(4, items[0].Quality);
         }
 
         [Test]
-        public void BackstagePassesQualityZeroAfterSellIn()
+        public void Quality_ForBackstagePassesAndZeroSellIn_IsZero()
         {
             var items = SetupTest(GildedRose.BACKSTAGE_PASS, 0, 1);
             Assert.AreEqual(0, items[0].Quality);
         }
 
         [Test]
-        public void ConjuredQualityDecreasesTwiceAsFase()
+        public void Quality_ForConjured_DecreasesTwiceAsFast()
         {
             var items = SetupTest(GildedRose.CONJURED, 1, 2);
             Assert.AreEqual(0, items[0].Quality);
         }
 
         [Test]
-        public void ConjuredQQualityDecreasesFourTimesAsFastIfSellInZero()
+        public void Quality_ForConjuredAndZeroSellIn_DecreasesFourTimesAsFast()
         {
             var items = SetupTest(GildedRose.CONJURED, 0, 4);
             Assert.AreEqual(0, items[0].Quality);
         }
 
         [Test]
-        public void ConjuredQQualityDecreasesFourTimesAsFastIfSellInNegative()
+        public void Quality_ForConjuredAndNegativeSellIn_DecreasesFourTimesAsFast()
         {
             var items = SetupTest(GildedRose.CONJURED, -1, 4);
             Assert.AreEqual(0, items[0].Quality);
